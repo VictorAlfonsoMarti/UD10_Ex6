ad0136c636816c739e5cdaaa74d7d12d51da3f9a
Víctor Alfonso Martí <victor19am@gmail.com>
2021-01-07 16:26:09 +0100

    07/01/2020 FINAL

---
+++ UD10_Ex6/UD10_Ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace UD10_Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro1 = new Libro(1111, "Libro 1", "Autor1", 100);
+            Libro libro2 = new Libro(2222, "Libro 2", "Autor2", 200);
+
+            libro1.ToString();
+            libro2.ToString();
+
+            Libro.MasPaginas(libro1, libro2);
+
+        }
+    }
+}
---
+++ UD10_Ex6/UD10_Ex6/dto/Libro.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10_Ex6
+{
+    class Libro
+    {
+        // ATRIBUTOS 
+        private int isbn;
+        private string titulo;
+        private string autor;
+        private int numPaginas;
+
+        // GETTERS Y SETTERS
+        public int _Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+        public string _Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        public string _Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        public int _NumPaginas
+        {
+            get { return numPaginas; }
+            set { numPaginas = value; }
+        }
+
+        // CONSTRUCTOR
+        public Libro(int isbn, string titulo, string autor, int numPaginas)
+        {
+            _Isbn = isbn;
+            _Titulo = titulo;
+            _Autor = autor;
+            _NumPaginas = numPaginas;
+        }
+
+        // METODOS
+        public override string ToString()
+        {
+            Console.WriteLine("El libro {0} con ISBN {1} creador por el autor {2} tiene {3} paginas.", this.titulo, this.isbn, this.autor, this.numPaginas );
+            return "";
+        }
+
+        public static void MasPaginas(Libro l1, Libro l2)
+        {
+            if (l1._NumPaginas > l2._NumPaginas)
+            {
+                Console.WriteLine("El libro {0} con {1} paginas, tiene más paginas que el libro {2} con {3} paginas", l1._Titulo, l1._NumPaginas, l2._Titulo, l2._NumPaginas);
+            }
+            else if (l1._NumPaginas == l2._NumPaginas)
+            {
+                Console.WriteLine("El libro {0} y el libro {1} tienen las mismas paginas: {2}", l1._Titulo, l2._Titulo, l2._NumPaginas);
+            }
+            else
+            {
+                Console.WriteLine("El libro {0} con {1} paginas, tiene más paginas que el libro {2} con {3} paginas", l2._Titulo, l2._NumPaginas, l1._Titulo, l1._NumPaginas);
+            }
+        }
+
+    }
+}
